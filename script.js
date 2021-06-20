ef4ec9348fd54a577014d3981334f338bbc6da67
Jonny <jonnya155@hotmail.com>
2021-06-20 21:26:24 +0100

    Function created that plays one round of the game and returns a message to the player depending on the outcome. computerPlay now returns strings as opposed to variable's to remove unnecessary code.

--- script.js
+++ script.js
@@ -1,19 +1,56 @@
 function computerPlay() {
-let one = 'Rock';
-let two = 'Paper';
-let three = 'Scissors';
 randomNumber = Math.ceil(Math.random()*3);
 
 if (randomNumber > 2) {
-    return three;
+    return 'Scissors';
 }
 else if (randomNumber > 1) {
-    return two;
+    return 'Paper';
 }
 else if (randomNumber <= 1) {
-    return one;
+    return 'Rock';
 }
 }
+
+// Randomly picks either rock, paper or scissors for the computer
+
+function playRound (playerSelection, computerSelection) {
+    let win = 'You win, well done!'
+    let lose = 'You lose, better luck next time.'
+    let draw = 'It\'s a draw!'
+
+    if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
+        return win;
+    }
+    else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
+        return lose;
+    }
+     else if (playerSelection === 'Rock' && computerSelection === 'Rock') {
+        return draw;
+    }
+    else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
+        return win;
+    }
+    else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
+        return lose;
+    }
+    else if (playerSelection === 'Paper' && computerSelection === 'Paper') {
+        return draw;
+    }
+    else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
+        return win;
+    }
+    else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
+        return lose;
+    }
+    else if (playerSelection === 'Scissors' && computerSelection === 'Scissors') {
+        return draw;
+    }
+}
+
+const playerSelection = 'Paper';
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
 console.log(computerPlay())
 
-// Picks either rock, paper or scissors for the computer
+// Plays one round of the game and returns a message to the player depending on the outcome
